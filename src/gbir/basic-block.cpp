defd0ed26e8872e956f5561549fd7959db777034
Lucas Ly Ba <hi@lucaslyba.com>
2026-07-13 00:09:39 +0200

    gbir: add basic block class
    * src/gbir/basic-block.cpp,
    * src/gbir/basic-block.h: Here.

---
+++ src/gbir/basic-block.cpp
@@ -0,0 +1,54 @@
+#include <gbir/basic-block.h>
+
+namespace gbir
+{
+    GbirBasicBlock::GbirBasicBlock(
+        std::string label, std::vector<std::unique_ptr<GbirInst>> instructions,
+        std::unique_ptr<GbirInst> terminator)
+        : label_(std::move(label))
+        , instructions_(std::move(instructions))
+        , terminator_(std::move(terminator))
+    {}
+
+    const std::string& GbirBasicBlock::label_get() const
+    {
+        return label_;
+    }
+
+    void GbirBasicBlock::label_set(std::string label)
+    {
+        label_ = std::move(label);
+    }
+
+    const std::vector<std::unique_ptr<GbirInst>>&
+    GbirBasicBlock::instructions_get() const
+    {
+        return instructions_;
+    }
+
+    std::vector<std::unique_ptr<GbirInst>>& GbirBasicBlock::instructions_get()
+    {
+        return instructions_;
+    }
+
+    void GbirBasicBlock::instructions_set(
+        std::vector<std::unique_ptr<GbirInst>> instructions)
+    {
+        instructions_ = std::move(instructions);
+    }
+
+    void GbirBasicBlock::add_instruction(std::unique_ptr<GbirInst> instruction)
+    {
+        instructions_.push_back(std::move(instruction));
+    }
+
+    GbirInst& GbirBasicBlock::terminator_get() const
+    {
+        return *terminator_;
+    }
+
+    void GbirBasicBlock::terminator_set(std::unique_ptr<GbirInst> terminator)
+    {
+        terminator_ = std::move(terminator);
+    }
+} // namespace gbir
---
+++ src/gbir/basic-block.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <gbir/inst/inst.h>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace gbir
+{
+    class GbirBasicBlock
+    {
+    public:
+        GbirBasicBlock(std::string label,
+                       std::vector<std::unique_ptr<GbirInst>> instructions,
+                       std::unique_ptr<GbirInst> terminator);
+
+        const std::string& label_get() const;
+        void label_set(std::string label);
+
+        const std::vector<std::unique_ptr<GbirInst>>& instructions_get() const;
+        std::vector<std::unique_ptr<GbirInst>>& instructions_get();
+        void
+        instructions_set(std::vector<std::unique_ptr<GbirInst>> instructions);
+        void add_instruction(std::unique_ptr<GbirInst> instruction);
+
+        GbirInst& terminator_get() const;
+        void terminator_set(std::unique_ptr<GbirInst> terminator);
+
+    private:
+        std::string label_;
+        std::vector<std::unique_ptr<GbirInst>> instructions_;
+        std::unique_ptr<GbirInst> terminator_;
+    };
+} // namespace gbir
